70a703167f62d8d4b54facccb15045df629781c2
Grégoire Ciles <gregoire.ciles@protonmail.com>
2023-12-24 11:00:44 +0100

    Rename file to autocmds

---
+++ dot_config/nvim/lua/butterfail/core/autocmds.lua
@@ -0,0 +1,26 @@
+-- AUTO COMMANDS
+local function augroup(name)
+  return vim.api.nvim_create_augroup("custom_au_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+  group = augroup("resize_splits"),
+  callback = function()
+    vim.cmd("tabdo wincmd =")
+  end,
+})
+
+-- Turn off paste mode when leaving insert
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = "*",
+  command = "set nopaste",
+})
+
+-- Disable the concealing in some file formats
+-- The default conceallevel is 3 in LazyVim
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "json", "jsonc", "markdown" },
+  callback = function()
+    vim.opt.conceallevel = 0
+  end,
+})
